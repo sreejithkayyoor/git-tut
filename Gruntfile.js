2cbe9bf80dfe2276e94792143fd75d28b315b815
Sreejith S <sreejithkayyoor@gmail.com>
2018-03-17 11:45:17 +0530

    Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,93 @@
+'use strict'
+
+module.exports= function(grunt){
+    
+    require('time-grunt')(grunt);
+    require('jit-grunt')(grunt);
+
+    grunt.initConfig({
+
+        sass: {
+            dist: {
+                files: {
+                    'css/styles.css':'css/styles.scss'
+                }
+            }
+        },
+
+
+        watch: {
+            files: 'css/*.scss',
+            tasks: ['sass']             
+        },
+
+            browserSync: {
+                dev: {
+                    bsFiles: {
+                        src: [
+                            'css/*.css',
+                            '*.html',
+                            'js/*.js'
+                        ]
+                    },
+                    options: {
+                        watchTask: true,
+                        server: {
+                            baseDir: './'
+                        }
+                    }
+                }
+            },
+            copy: {
+                html: {
+                    files: [{
+                        expand: true,
+                        dot: true,
+                        cwd: './',
+                        src: ['*.html'],
+                        dest: 'dist'
+                    }]
+                },
+                css:{
+                    expand:true,
+                    dont: true,
+                    cwd: './',
+                    src: 'css/*.css',
+                    dest: 'dist'
+                },
+                fonts: {
+                    files: [{
+                        expand: true,
+                        dot: true,
+                        cwd: 'node_modules/font-awesome',
+                        src: 'fonts/*.*',
+                        dest: 'dist'
+                    }]
+                }
+            },
+            clean: {
+                build:{
+                    src: ['dist/']
+                }
+            },
+            imagemin: {
+                dynamic: {
+                    files: [{
+                        expand: true,
+                        cwd: './',
+                        src: ['img/*.{png,gif,jpg}'],
+                        dest: 'dist/'
+                    }]
+                }
+            }
+    });
+
+    grunt.registerTask('css',['sass']);
+    grunt.registerTask('default',['browserSync', 'watch']);
+    grunt.registerTask('build',[
+        'clean',
+        'copy',
+        'imagemin'
+    ]);
+
+}
